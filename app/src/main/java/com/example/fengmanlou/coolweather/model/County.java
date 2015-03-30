bf0f89e9ba1a26fc410d76c6cb8af34808af0519
hys <1211904437@qq.com>
2015-03-30 10:10:51 +0800

    删除了MainActivity，增加了数据库操作类

---
+++ app/src/main/java/com/example/fengmanlou/coolweather/model/County.java
@@ -0,0 +1,43 @@
+package com.example.fengmanlou.coolweather.model;
+
+/**
+ * Created by fengmanlou on 2015/3/30.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
